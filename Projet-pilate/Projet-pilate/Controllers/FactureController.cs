c30a0119c791f706f3da624cd52fc432b8fa93a2
xinyu <fengxy233@gmail.com>
2020-07-10 09:42:59 +0200

    listeFactures

---
+++ Projet-pilate/Projet-pilate/Controllers/FactureController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Projet_pilate.Models;
+using System.Web.Mvc;
+using System.Globalization;
+
+namespace Projet_pilate.Controllers
+{
+    public class FactureController : Controller
+    {
+        // GET: Facture
+        [Route("Facture/ListeFactures", Name = "ListeFactures")]
+        public ActionResult ListeFactures()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            ViewBag.date = db.MonthActivations.Single().Periode;
+
+            List<FactureSimpleViewModel> models = new List<FactureSimpleViewModel>();
+            var factureList = db.Factures.ToList();
+
+            foreach(var f in factureList)
+            {
+                FactureSimpleViewModel model = new FactureSimpleViewModel()
+                {
+                    ID = f.FactureID,
+                    NomFacture = f.NomFacture,
+                    Client = f.Client,
+                    MontantHT = f.MontantHT.ToString(),
+                    FAE = f.FAE==false?"NON":"OUI",
+                    Emise = f.Emise == false ? "NON" : "OUI",
+                    Payee = f.payee == false ? "NON" : "OUI",
+                    Annulee = f.annulee == false ? "NON" : "OUI",
+                };
+                models.Add(model);
+            
+            }
+
+
+            return View(models);
+        }
+
+        [Route("Facture/Return", Name = "Return")]
+        public ActionResult Return()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            ClotureViewModel model = new ClotureViewModel();
+            var Mois = db.MonthActivations.Single();
+            model.MoisaCloturer = Mois.Periode.ToString("MMMM", CultureInfo.CurrentCulture) +
+                              " " + Mois.Periode.Year.ToString();
+
+            ModelState.AddModelError(string.Empty, "Mois a été clôturé");
+            return View("ClotureMois","Admin", model);
+        }
+
+        /*[Route("Facture/ListeFactures", Name = "ListeFactures")]
+        public ActionResult ListeFactures()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            ViewBag.date = db.MonthActivations.Single().Periode;
+            return View();
+        }*/
+
+        [Route("Facture/Suivis", Name = "Suivis")]
+        public ActionResult Suivis()
+        {
+            return View();
+        }
+
+        [Route("Facture/Modifier", Name = "Modifier")]
+        public ActionResult Modifier()
+        {
+            return View();
+        }
+
+        [Route("Facture/Detail", Name = "Detail")]
+        public ActionResult Detail()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Projet-pilate/Projet-pilate/Models/FactureViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Projet_pilate.Models
+{
+    public class FactureSimpleViewModel
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Nom du Facture")]
+        public string NomFacture { get; set; }
+
+        [Display(Name = "Client")]
+        public string Client { get; set; }
+
+        [Display(Name = "Montant")]
+        public string MontantHT { get; set; }
+
+        [Display(Name = "FAE")]
+        public string FAE { get; set; }
+
+        [Display(Name = "Emise")]
+        public string Emise { get; set; }
+
+        [Display(Name = "Payee")]
+        public string Payee { get; set; }
+
+        [Display(Name = "Annulee")]
+        public string Annulee { get; set; }
+
+    }
+
+}
